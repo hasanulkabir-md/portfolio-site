1b733a0e74aa0d5f59d43b23bcbff13fa30b3ba8
hasanulkabir-md <70855211+hasanulkabir-md@users.noreply.github.com>
2025-09-23 17:16:57 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-<script>
-  function toggleTheme() {
-    const html = document.documentElement;
-    const current = html.getAttribute("data-theme");
-    html.setAttribute("data-theme", current === "light" ? "dark" : "light");
-  }
-</script>
