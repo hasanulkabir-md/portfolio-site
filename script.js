4f5dfc7a7cca2164d4af20d25dd01c0abe79cb94
hasanulkabir-md <70855211+hasanulkabir-md@users.noreply.github.com>
2025-09-23 17:19:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+  function toggleTheme() {
+    const html = document.documentElement;
+    const current = html.getAttribute("data-theme");
+    html.setAttribute("data-theme", current === "light" ? "dark" : "light");
+  }
+</script>
